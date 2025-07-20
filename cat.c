8df895eae5f9c56a1068ca8e2e291d317eea4146
sachin kumar <155023931+justsachin-kumar@users.noreply.github.com>
2025-07-20 13:37:31 +0530

    Update cat.c

--- cat.c
+++ cat.c
@@ -1,20 +1,24 @@
+// header files
 #include <stdio.h>
 #include <string.h>
-
+// main with command line argument
 int main(int argc, char *argv[])
 {
+    // conditions
     if(argc != 2)
     {
         printf("Usage: ./cat <filename>\n");
         return 1;
     }
-
+    // file pointer
     FILE *file = fopen(argv[1], "r");
+    
     if(file == NULL)
     {
         printf("file doesn't exist\n");
         return 1;
     }
+    // loop to iterate character using fgetc() function
     char ch;
 
     while ((ch = fgetc(file)) != EOF)
